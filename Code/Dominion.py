fca4d8276bd826591ccddd3c644d3726904e0e48
JahnaeE <106354720+JahnaeE@users.noreply.github.com>
2023-11-02 16:36:47 -0400

    Dominion.py

--- Code/Dominion.py
+++ Code/Dominion.py
@@ -1,10 +1,8 @@
-#packages to indtall
-#!pip install transformers==4.30.2
 #!pip install h5py
 #!pip install typing-extensions
 #!pip install wheel
 #!pip install flair
-
+#!pip install transformers == 4.30.2
 # with larger model
 #*** ENSURE YOU PIP INSTALL 'transformers==4.30.2' ****
 from flair.data import Sentence
@@ -23,14 +21,11 @@ nltk.download('averaged_perceptron_tagger')
 #xlm_roberta_tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
 
 
-#tweets = pd.read_csv('C:/Users/diego/OneDrive/Desktop/BotTest/NER_TEST_FILES/Input data/oct28_nov12_combined_data.csv', low_memory=False, encoding='latin-1')
-tweets = pd.read_csv('combined.csv', low_memory=False, encoding='latin-1')
-tweets = tweets.iloc[0:1000]
-#tweets = tweets.iloc[0:8723]
+tweets = pd.read_csv('C:/Users/13212/OneDrive/Documents/College/USF/GA Position/Dominon/combined.csv', low_memory=False, encoding='latin-1')
 
 #delete na
 tweets = tweets.dropna(subset=['Title'])
-
+tweets = tweets.iloc[1:20]
 
 #create functions
 def engineer_sentence(entity, tag, tweet, index=0):
@@ -53,7 +48,7 @@ def create_ner_df(lis):
     dates_ = []
     engineered_sentences = []
     index = 0
-    for tweet in lis:
+    for tweet, date in zip(lis, tweets['Date']):
         print(index)
         # tokenizes sentence to preform different n-grams
         sentence = Sentence(tweet)
@@ -71,8 +66,7 @@ def create_ner_df(lis):
             tweet_id_.append(index)
             entity_.append(label.data_point.text)
             tag_.append(label.value)
-
-
+            dates_.append(date)
 
         engineered_sentences.append(replace)
         index = index + 1
@@ -81,7 +75,7 @@ def create_ner_df(lis):
     engineered_sentences_df = pd.DataFrame.from_dict(engineered_sentences_df, orient='index')
     engineered_sentences_df = engineered_sentences_df.transpose()
 
-    count_df = {"tweet_id": tweet_id_, 'entity': entity_, 'tag': tag_}
+    count_df = {"tweet_id": tweet_id_, 'entity': entity_, 'tag': tag_, 'date': dates_}
     count_df = pd.DataFrame.from_dict(count_df, orient='index')
     count_df = count_df.transpose()
     return engineered_sentences_df, count_df
@@ -89,8 +83,5 @@ def create_ner_df(lis):
 #run functions
 engineered_sentences_df, count_df = create_ner_df(tweets['Title'].to_list())
 
-#count_df.to_csv('C:/Users/diego/OneDrive/Desktop/BotTest/NER_TEST_FILES/large_model_Ner Output/large_ner_output.csv', index = None)
-#engineered_sentences_df.to_csv('C:/Users/diego/OneDrive/Desktop/BotTest/NER_TEST_FILES/Labelled sentence/large_labelled_sentence_output.csv', index = None)
-
-count_df.to_csv('/content/ner_output.csv', index = None)
-engineered_sentences_df.to_csv('/content/labelled_sentence_output.csv', index = None)
+count_df.to_csv('C:/Users/13212/OneDrive/Documents/College/USF/GA Position/Dominon/raw NER/large_ner_output.csv', index = None)
+engineered_sentences_df.to_csv('C:/Users/13212/OneDrive/Documents/College/USF/GA Position/Dominon/raw NER/large_labelled_sentence_output.csv', index = None)
